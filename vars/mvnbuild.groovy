d6cadd64a4572a270c8498cd4601eb7fc793d6fa
dvamsikrishna <vamsikrishnad.oracle@gmail.com>
2021-03-09 22:44:50 +0530

    Update mvnbuild.groovy

--- vars/mvnbuild.groovy
+++ vars/mvnbuild.groovy
@@ -3,6 +3,8 @@
 def call(String name) {
 node('master') {
 				echo 'maven clean'
-	bat "mvn -f pom.xml clean install"
+	bat """
+	cd "C:\\Program Files (x86)\\Jenkins\\workspace\\Shared-lib-Pipeline\\git_url"
+	mvn -f pom.xml clean install"
 }
 }
